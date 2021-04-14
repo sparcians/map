b71a2242a9aa185a24d40e842d6fb43be7e006cc
Edward Brekelbaum <edward.brekelbaum@sifive.com>
2021-04-14 08:41:08 -0700

    Adjusted logic for upper_bound (overflow now falls into the last bucket)
    Added template argument to assert on underflow
    
    Assert bucket specification is sorted

--- sparta/sparta/statistics/BasicHistogram.hpp
+++ sparta/sparta/statistics/BasicHistogram.hpp
@@ -11,18 +11,24 @@
 namespace sparta
 {
 /// Histogram with programmable buckets
+template<typename T, bool ASSERT_ON_UNDERFLOW>
 class BasicHistogram
 {
 public:
     /// constructor
     ///\param buckets one bucket will be created per value (plus one for overflow) - values must be sorted
-    BasicHistogram(sparta::StatisticSet &sset, const std::string &name, const std::string &desc, const std::vector<int64_t> &buckets)
+    BasicHistogram(sparta::StatisticSet &sset,
+                   const std::string &name,
+                   const std::string &desc,
+                   const std::vector<T> &buckets)
     : bucket_vals_(buckets)
     {
+        sparta_assert(std::is_sorted(buckets.begin(), buckets.end()), "Buckets must be sorted");
+
         // create one counter per bucket
         for (unsigned i = 0; i < bucket_vals_.size(); ++i)
         {
-            const int64_t v = bucket_vals_[i];
+            const T &v = bucket_vals_[i];
             std::ostringstream os_name;
             if (v < 0)
                 os_name << name << "_n" << -v; // negative
@@ -41,25 +47,28 @@ public:
 
             ctrs_.push_back(sparta::Counter(&sset, os_name.str(), os_desc.str(), sparta::Counter::COUNT_NORMAL));
         }
-
-        // create a final bucket for everything beyond the last value
-        std::ostringstream os_name;
-        os_name << name << "_of";
-        std::ostringstream os_desc;
-        os_desc << desc << " overflow";
-        ctrs_.push_back(sparta::Counter(&sset, os_name.str(), os_desc.str(), sparta::Counter::COUNT_NORMAL));
     }
 
     /// increment the bucket corresponding to 'val'
-    void addValue(int64_t val)
+    void addValue(const T &val)
     {
+        // upper_bound will yield the bucket beyond the one we want
         auto bucket = std::upper_bound(bucket_vals_.begin(), bucket_vals_.end(), val);
-        auto off = bucket - bucket_vals_.begin();
+
+        // check for underflow (value below first bucket)
+        if (bucket == bucket_vals_.begin())
+        {
+            sparta_assert(!ASSERT_ON_UNDERFLOW, "Value below first bucket");
+            ++ctrs_[0]; // put underflow in first bucket
+            return;
+        }
+        //else calculate offset into counter array
+        auto off = bucket - bucket_vals_.begin() - 1;
         ++ctrs_[off];
     }
 
 private: // data
-    std::vector<int64_t> bucket_vals_; ///< user-specified buckets
+    std::vector<T> bucket_vals_; ///< user-specified buckets
     std::vector<sparta::Counter> ctrs_; ///< one counter per bucket
 };
 } // namespace sparta
