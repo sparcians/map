299c7f79f7b54a372afee6712367b82f68903185
Rob Bell <robert.bell@samsung.com>
2021-09-29 10:09:55 -0500

    Add hooks for build and configure in ResourceTreeNode.

--- sparta/sparta/simulation/ResourceTreeNode.hpp
+++ sparta/sparta/simulation/ResourceTreeNode.hpp
@@ -328,8 +328,8 @@ namespace sparta
          * \brief When the framework is entering the configuration phase
          */
         void onConfiguring_() override {
-            // Create subtree, which may want to look at parameters
-            res_fact_->createSubtree(this);
+            // Hook to configure mode, even call create subtree
+            res_fact_->onConfiguring(this);
         }
 
         /*!
@@ -374,6 +374,9 @@ namespace sparta
             sparta_assert(params_ != 0); // Params must not be NULL
             params_->resetReadCounts(); // Reset read-counts to 0
 
+            //Hook for additional building is needed
+            res_fact_->onBuilding(this);
+            
             std::string res_type = res_fact_->getResourceType();
             if(res_type == ""){
                 throw SpartaException("resource type for ResourceFactory associated with ")
