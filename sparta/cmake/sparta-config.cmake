cc3fb8aec1fbe4564f6b24bd75e0b3ad34bb741d
Knute Lingaard <Knute.Lingaard@sifive.com>
2022-02-24 12:52:18 -0600

    Force-set the YAML include path

--- sparta/cmake/sparta-config.cmake
+++ sparta/cmake/sparta-config.cmake
@@ -60,6 +60,7 @@ message (STATUS "Using BOOST ${Boost_VERSION_STRING}")
 # Find YAML CPP
 find_package (yaml-cpp 0.6 REQUIRED)
 message (STATUS "Using YAML CPP ${yaml-cpp_VERSION}")
+get_target_property(YAML_CPP_INCLUDE_DIR yaml-cpp INTERFACE_INCLUDE_DIRECTORIES)
 
 # Find RapidJSON
 find_package (RapidJSON 1.1 REQUIRED)
