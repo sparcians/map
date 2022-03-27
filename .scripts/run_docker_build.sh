519f6fa898a76d300d9e9885beb0982a38a9b6c4
Tim Snyder <snyder.tim@gmail.com>
2022-03-27 18:23:59 +0000

    perhaps default image can't run docker anymore?

--- .scripts/run_docker_build.sh
+++ .scripts/run_docker_build.sh
@@ -76,9 +76,7 @@ fi
 
 export UPLOAD_PACKAGES="${UPLOAD_PACKAGES:-True}"
 export IS_PR_BUILD="${IS_PR_BUILD:-False}"
-if [[ "$CI" != circle ]]; then
-    docker pull "${DOCKER_IMAGE}"
-fi
+docker pull "${DOCKER_IMAGE}"
 docker run ${DOCKER_RUN_ARGS} \
            -v "${RECIPE_ROOT}":/home/conda/recipe_root:rw,z,delegated \
            -v "${FEEDSTOCK_ROOT}":/home/conda/feedstock_root:rw,z,delegated \
