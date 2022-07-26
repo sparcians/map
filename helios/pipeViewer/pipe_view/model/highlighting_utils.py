bf813afb29443d6524c25d8f3678052d7b4ddf51
Brett Dutro <brett.dutro@sifive.com>
2022-07-26 13:26:16 -0700

    argos: Use re.search instead of re.match when looking for `uid` field in annotation     - Fixes bug where multiline annotations broke uop highlighting

--- helios/pipeViewer/pipe_view/model/highlighting_utils.py
+++ helios/pipeViewer/pipe_view/model/highlighting_utils.py
@@ -7,7 +7,7 @@ def GetUopUid(anno_string):
     if not isinstance(anno_string, str):
         return None
 
-    uid_match = __UOP_UID_REGEX.match(anno_string)
+    uid_match = __UOP_UID_REGEX.search(anno_string)
 
     if uid_match is not None:
         return int(uid_match.group(1))
