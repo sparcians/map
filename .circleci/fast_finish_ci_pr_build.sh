190886d06e17f93439011572c0510ecd09287676
Knute Lingaard <klingaard@gmail.com>
2025-04-18 16:02:40 -0500

    Replaced Azure/CircleCI with GitHub actions

--- .circleci/fast_finish_ci_pr_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/conda-forge/conda-forge-ci-setup-feedstock/main/recipe/conda_forge_ci_setup/ff_ci_pr_build.py | \
-     python - -v --ci "circle" "${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_PR_NUMBER}"
---
+++ .github/actions/entrypoint.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -x
+
+echo "Starting Build Entry"
+echo "HOME:" $HOME
+echo "GITHUB_WORKSPACE:" $GITHUB_WORKSPACE
+echo "GITHUB_EVENT_PATH:" $GITHUB_EVENT_PATH
+echo "PWD:" `pwd`
+
+CXX_COMPILER=${COMPILER/clang/clang++}
+CXX_COMPILER=${CXX_COMPILER/gcc/g++}
+
+#
+# Compile Sparta Infra (always build with release)
+#   Have other build types point to release
+#
+echo "Building Sparta Infra"
+cd ${GITHUB_WORKSPACE}/map/sparta
+mkdir -p release
+cd release
+CC=$COMPILER CXX=$CXX_COMPILER  cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr/local/
+if [ $? -ne 0 ]; then
+    echo "ERROR: CMake for Sparta framework failed"
+    exit 1
+fi
+
+sudo make -j$(nproc --all) install
+BUILD_SPARTA=$?
+if [ ${BUILD_SPARTA} -ne 0 ]; then
+    echo "ERROR: build/install for sparta FAILED!!!"
+    echo "$(<install.log)"
+    exit 1
+fi
+rm install.log
+
+if [ "${BUILD_TYPE}" = "Release"]; then
+    make -j$(nproc --all) regress
+    if [ $? -ne 0 ]; then
+        echo "ERROR: regress of sparta FAILED!!!"
+        exit 1
+    fi
+
+    make -j$(nproc --all) example_regress
+    if [ $? -ne 0 ]; then
+        echo "ERROR: example_regress of sparta FAILED!!!"
+        exit 1
+    fi
+
+    make -j$(nproc --all) core_example_regress
+    if [ $? -ne 0 ]; then
+        echo "ERROR: example_regress of sparta FAILED!!!"
+        exit 1
+    fi
+fi
