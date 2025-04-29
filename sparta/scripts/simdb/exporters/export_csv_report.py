1c15cc271a8eb9544158e68355c364bee650183c
Colby Nyce <iheartridleyman@gmail.com>
2025-04-29 10:07:39 -0500

    CSV exporter (SimDB): Write report header

--- sparta/scripts/simdb/exporters/export_csv_report.py
+++ sparta/scripts/simdb/exporters/export_csv_report.py
@@ -3,8 +3,34 @@ class CSVReportExporter:
         pass
 
     def Export(self, dest_file, descriptor_id, db_conn):
-        # For now, just "touch" each report file. The comparison script will naturally
-        # fail which is okay for now.
-        with open(dest_file, "w") as fout:
-            print (f"Exporting {dest_file}...")
-            fout.write("# This is a placeholder file. The SimDB exporter is not implemented yet.\n")
+        cursor = db_conn.cursor()
+        cmd = f'SELECT Name,StartTick,EndTick FROM Reports WHERE ReportDescID={descriptor_id} AND ParentReportID=0'
+        cursor.execute(cmd)
+        report_name, start_tick, end_tick = cursor.fetchone()
+
+        cmd = f'SELECT MetaName, MetaValue FROM ReportMetadata WHERE ReportDescID={descriptor_id} '
+        cmd += 'AND MetaName NOT IN (\'OmitZeros\', \'PrettyPrint\')'
+        cursor.execute(cmd)
+
+        meta_kvpairs = []
+        for meta_name, meta_value in cursor.fetchall():
+            meta_kvpairs.append((meta_name, meta_value))
+
+        with open(dest_file, 'w') as fout:
+            self.__WriteHeader(fout, report_name, start_tick, end_tick, meta_kvpairs)
+
+    def __WriteHeader(self, fout, report_name, start_tick, end_tick, meta_kvpairs):
+        if end_tick == -1:
+            end_tick = 'SIMULATION_END'
+        fout.write(f'# report="{report_name}",start={start_tick},end={end_tick}')
+
+        if not meta_kvpairs:
+            fout.write('\n')
+            return
+
+        for i, (meta_name, meta_value) in enumerate(meta_kvpairs):
+            meta_kvpairs[i] = f'{meta_name}={meta_value}'
+
+        fout.write(',')
+        fout.write(','.join(meta_kvpairs))
+        fout.write('\n')
