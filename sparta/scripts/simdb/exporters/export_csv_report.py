4787fe2fe575aa2db71a4639a26a3d69c3c801a8
colby-nyce <iheartridleyman@gmail.com>
2025-05-07 10:49:15 -0500

    Get all SimDB-exported CSV reports to match MAP v2 formatters exactly (#565)
    * Bug fixes for SimDB report verif scripts
    
    * Append report_format to the SimDB exported CSV reports
    
    * Copy report yamls to the rerun dir (sim.cmd / sim.rerun.cmd)
    
    * Update the start tick when triggered (simdb reports)
    
    * Add start/stop/update counter locations to the CSV header
    
    * Add start/stop/update counter locations to the CSV header
    
    * Add start/stop/update counter locations to the CSV header
    
    * Add more SimDB report tests (csv)
    
    * Do not overwrite report start tick on 2nd+ toggle trigger
    
    * SimDB csv reports: skip over rows when toggle trigger was disabled
    
    * Utility to parse CMakeLists.txt into 60 test cases
    
    * Touch script file
    
    * PR feedback

--- sparta/scripts/simdb/exporters/export_csv_report.py
+++ sparta/scripts/simdb/exporters/export_csv_report.py
@@ -16,14 +16,45 @@ class CSVReportExporter:
 
         cmd = f'SELECT MetaName, MetaValue FROM ReportMetadata WHERE ReportDescID={descriptor_id} '
         cmd += 'AND MetaName NOT IN (\'OmitZeros\', \'PrettyPrint\')'
+
+        # Sort alphabetically by name to match std::map<string, string> in C++.
+        cmd += ' ORDER BY MetaName ASC'
+
         cursor.execute(cmd)
 
         meta_kvpairs = []
         for meta_name, meta_value in cursor.fetchall():
             meta_kvpairs.append((meta_name, meta_value))
 
+        # Force the "report_format" metadata to be visible at the top of the CSV file.
+        has_report_format = False
+        for meta_name, meta_value in meta_kvpairs:
+            if meta_name == 'report_format':
+                has_report_format = True
+                break
+
+        if not has_report_format:
+            cmd = f'SELECT Format FROM ReportDescriptors WHERE Id={descriptor_id}'
+            cursor.execute(cmd)
+            format = cursor.fetchone()[0]
+            meta_kvpairs.append(('report_format', format))
+
+        # We write multi-line headers for start/stop/update counter locations.
+        trigger_locs = []
+        cmd = 'SELECT StartCounter, StopCounter, UpdateCounter FROM Reports WHERE '
+        cmd += f'ReportDescID={descriptor_id} AND ParentReportID=0'
+        cursor.execute(cmd)
+
+        start_counter_loc, stop_counter_loc, update_counter_loc = cursor.fetchone()
+        if start_counter_loc:
+            trigger_locs.append(('start_counter', start_counter_loc))
+        if stop_counter_loc:
+            trigger_locs.append(('stop_counter', stop_counter_loc))
+        if update_counter_loc:
+            trigger_locs.append(('update_counter', update_counter_loc))
+
         with open(dest_file, 'w') as fout:
-            self.__WriteHeader(fout, report_name, start_tick, end_tick, meta_kvpairs)
+            self.__WriteHeader(fout, report_name, start_tick, end_tick, meta_kvpairs, trigger_locs)
 
         # Now go through this descriptor's reports/subreports, and get an ordered list of
         # the statistics that are in the report. This is line #2 of the CSV file (column
@@ -40,9 +71,20 @@ class CSVReportExporter:
         # row in the CSV report (one SQL record only holds the values for the same report descriptor).
         with open(dest_file, 'a') as fout:
             basename = os.path.basename(dest_file)
-            cmd = f'SELECT Data, IsCompressed FROM CollectionRecords WHERE Notes=\'{basename}\' ORDER BY Tick ASC'
+            cmd = f'SELECT Data, IsCompressed, Notes FROM CollectionRecords WHERE Notes LIKE \'{basename}%\' ORDER BY Tick ASC'
             cursor.execute(cmd)
-            for data, is_compressed in cursor.fetchall():
+            for data, is_compressed, notes in cursor.fetchall():
+                # If the Notes column is something like "out.csv_skipped_anno#1033", that means we
+                # should write a row of empty values for this record to denote the number of times
+                # we skipped writing to the report.
+                if notes.find('skipped_anno') != -1:
+                    hash_idx = notes.find('#')
+                    anno = notes[hash_idx:]
+                    anno += ','*(len(stat_headers) - 1)
+                    fout.write(anno)
+                    fout.write('\n')
+                    continue
+
                 if is_compressed:
                     data = zlib.decompress(data)
 
@@ -67,7 +109,7 @@ class CSVReportExporter:
                 fout.write(','.join(row_values))
                 fout.write('\n')
 
-    def __WriteHeader(self, fout, report_name, start_tick, end_tick, meta_kvpairs):
+    def __WriteHeader(self, fout, report_name, start_tick, end_tick, meta_kvpairs, trigger_locs):
         if end_tick == -1:
             end_tick = 'SIMULATION_END'
         fout.write(f'# report="{report_name}",start={start_tick},end={end_tick}')
@@ -82,6 +124,15 @@ class CSVReportExporter:
         fout.write(',')
         fout.write(','.join(meta_kvpairs))
         fout.write('\n')
+
+        # Write the trigger locations if provided
+        if trigger_locs:
+            fout.write('# ')
+            strs = []
+            for name, loc in trigger_locs:
+                strs.append(f'{name}={loc}')
+            fout.write(','.join(strs))
+            fout.write('\n')
 
     def __RecurseGetStatHeaders(self, cursor, report_id, prefix, stat_headers):
         cmd = f'SELECT StatisticName, StatisticLoc FROM StatisticInsts WHERE ReportID={report_id}'
