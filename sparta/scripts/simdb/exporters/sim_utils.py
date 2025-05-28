576176fbda59e0a84a3ea9ce4b701cf264a37541
colby-nyce <iheartridleyman@gmail.com>
2025-05-28 17:01:01 -0500

    Install SimDB export/comparator scripts for MAP v2.1 release (#578)
    * Install SimDB export/comparator scripts for MAP v2.1 release
    
    * Fix report unit test (no simulation)
    
    * Touch simdb_export.py
    
    * Fix SimInfo report discrepancies. Do not save reports from destructor.
    
    * PR feedback
    
    * Improve CSV export perf by 10%

--- sparta/scripts/simdb/exporters/sim_utils.py
+++ sparta/scripts/simdb/exporters/sim_utils.py
@@ -4,7 +4,7 @@ class SimulationInfo:
     @classmethod
     def GetHeaderPairs(cls, db_conn):
         cursor = db_conn.cursor()
-        cmd = "SELECT HeaderName, HeaderValue FROM SimulationInfoHeaderPairs"
+        cmd = "SELECT HeaderName, HeaderValue FROM SimulationInfoHeaderPairs WHERE HeaderName != 'Other'"
         cursor.execute(cmd)
 
         header_pairs = []
@@ -25,6 +25,19 @@ class SimulationInfo:
 
             # out << p.second << line_end;
             out.write(f"{p[1]}{line_end}")
+
+        # if(other.size() > 0){
+        #     o << line_start << "Other:" << line_end;
+        #     for(const std::string& oi : other){
+        #         o << line_start << "  " << oi << line_end;
+        #     }
+        # }
+        cmd = "SELECT HeaderValue FROM SimulationInfoHeaderPairs WHERE HeaderName == 'Other'"
+        cursor = db_conn.cursor()
+        cursor.execute(cmd)
+
+        for row in cursor.fetchall():
+            out.write(f"{line_start}Other:  {row[0]}{line_end}")
 
 def GetSimInfo(cursor):
     cmd = "SELECT SimName, SimVersion, SpartaVersion, ReproInfo FROM SimulationInfo"
