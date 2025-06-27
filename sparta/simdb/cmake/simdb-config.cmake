954d7a4cf4f077cee3ed7474525986fb24e4976f
Brett Dutro <brett.dutro@gmail.com>
2025-06-27 14:30:52 -0500

    Use correct RapidJSON include paths Fix failure when trying to build against an HDF5 library with MPI support enabled Fix compiler error with Clang 20+ and GCC 15+

--- sparta/simdb/cmake/simdb-config.cmake
+++ sparta/simdb/cmake/simdb-config.cmake
@@ -11,3 +11,9 @@ enable_language(C)
 find_package(HDF5 1.10 REQUIRED)
 
 set(SimDB_LIBS simdb ${HDF5_LIBRARIES} sqlite3 z pthread)
+
+# If HDF5 is built with MPI support, we also need to add the MPI include dirs and link against the MPI library
+if(HDF5_IS_PARALLEL)
+    find_package(MPI REQUIRED)
+    list(APPEND SimDB_LIBS MPI::MPI_CXX)
+endif()
