e06cf7a841a21f436be08f69a99c3d72f19c06e2
Knute Lingaard (MIPS) <155678575+knute-mips@users.noreply.github.com>
2025-12-12 16:08:59 -0600

    A little cleanup for systemc builds (#630)
    Made sure we're compiling with systemc 2.3. Also cleaned up
    documentation a bit and added more descriptions.

--- sparta/example/SystemC/common/SpartaSystemCSimulator.hpp
+++ sparta/example/SystemC/common/SpartaSystemCSimulator.hpp
@@ -11,10 +11,25 @@ namespace sparta {
 
 namespace sparta_sim
 {
-
+    //! \class SpartaSystemCSimulator
+    //! \brief Top level constructor for the SystemC example TLM simulator
     class SpartaSystemCSimulator : public sparta::app::Simulation
     {
     public:
+        /**
+         * \brief Construction of the SpartaSystemCSimulator
+         *
+         * This class creates the following components for a simple
+         * simulation:
+         *
+         * - `systemc_example_top` that contains the `tlm::SimpleBusAT` and the two `tlm::initiator_top` instances
+         * - `sparta::SpartaTLMTargetGasket` that binds to SystemC on one side and Sparta on the other
+         * - `sparta::SpartaMemory` which is the receiver of traffic from the initiators
+         * - `sparta::SysCSpartaSchedulerAdapter` which will run the simulator including the SystemC kernel
+         *
+         * Then, when asked to run, will call run on the
+         * SysCSpartaSchedulerAdapter which runs both kernels
+         */
         SpartaSystemCSimulator(sparta::Scheduler * sched);
 
         ~SpartaSystemCSimulator();
