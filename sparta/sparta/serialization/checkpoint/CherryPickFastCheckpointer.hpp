71f36a68a7c8c40ffa9420ca36c6a4194de8297f
Colby Nyce <iheartridleyman@gmail.com>
2026-02-21 14:29:09 -0600

    Changes to support CoSim event replayer

--- sparta/sparta/serialization/checkpoint/CherryPickFastCheckpointer.hpp
+++ sparta/sparta/serialization/checkpoint/CherryPickFastCheckpointer.hpp
@@ -6,6 +6,7 @@
 #include "sparta/utils/SpartaException.hpp"
 #include "simdb/apps/App.hpp"
 #include "simdb/utils/ConcurrentQueue.hpp"
+#include "simdb/sqlite/Iterator.hpp"
 #include <map>
 
 namespace sparta
@@ -119,13 +120,130 @@ public:
      *
      *   If true, then everything from S1 to D5 will be saved to disk, and a new
      *   head checkpoint S3 will be created in memory at the current tick.
+     *
+     * \param committed_checkpoints If not nullptr, this vector will be filled with
+     * the IDs of the checkpoints that were sent down the pipeline.
      */
-    void commitCurrentBranch(bool force_new_head_chkpt = false);
+    void commitCurrentBranch(bool force_new_head_chkpt = false,
+                             std::vector<chkpt_id_t>* committed_checkpoints = nullptr);
 
     /*!
      * \brief Send the committed checkpoints down the pipeline to the database.
      */
     void saveCheckpoints(checkpoint_ptrs&& checkpoints);
+
+    /*!
+     * \brief Called at the end of simulation just prior to shutting down threads
+     */
+    void preTeardown() override;
+
+    //! \brief Forward declaration for DatabaseCheckpointLoader
+    struct ChkptWindow;
+
+    //! \brief Arch ID range at a particular tick
+    struct ArchIdsForTick {
+        arch_id_t start_arch_id = UINT64_MAX;
+        arch_id_t end_arch_id = UINT64_MAX;
+        tick_t tick = UINT64_MAX;
+    };
+
+    /*!
+     * \class DatabaseCheckpointLoader
+     * \brief This class retrieves checkpoints from disk for post-simulation
+     * use cases.
+     */
+    class DatabaseCheckpointLoader
+    {
+    public:
+        //! \brief Construct with one root from which we'll find the ArchData's
+        DatabaseCheckpointLoader(simdb::DatabaseManager* db_mgr, TreeNode* root) :
+            DatabaseCheckpointLoader(db_mgr, std::vector<TreeNode*>({root}))
+        {}
+
+        //! \brief Construct with multiple roots from which we'll find the ArchData's
+        DatabaseCheckpointLoader(simdb::DatabaseManager* db_mgr, const std::vector<TreeNode*>& roots);
+
+        //! \brief Get the total number of committed checkpoints
+        //! \note This is used as the max value for the linear index
+        //! passed to loadCheckpoint()
+        uint64_t getNumCheckpoints() const { return num_checkpoints_; }
+
+        //! \brief Load a checkpoint with an index between [0,getNumCheckpoints())
+        //! \throw Throws if index is out of range
+        void loadCheckpoint(uint64_t checkpoint_idx) {
+            if (checkpoint_idx >= num_checkpoints_) {
+                throw SpartaException("Checkpoint index out of range");
+            }
+            loadCheckpoint_(checkpoint_idx);
+        }
+
+    private:
+        //! \brief Find all ArchData's from the given roots (recursive)
+        static std::vector<ArchData*> enumerateArchDatas_(const std::vector<TreeNode*>& roots);
+
+        //! \brief Get the exact checkpoint from the given window
+        static checkpoint_type* getChkptFromWindow_(arch_id_t arch_id, tick_t tick, ChkptWindow& window);
+
+        //! \brief Get the exact checkpoint from our cache
+        checkpoint_type* getChkptFromCache_(arch_id_t arch_id, tick_t tick);
+
+        //! \brief Go to the database looking for the exact checkpoint with
+        //! this arch ID and tick
+        //! \note If found, this will overwrite the cached window
+        checkpoint_type* getChkptFromDisk_(arch_id_t arch_id, tick_t tick);
+
+        //! \brief Load a checkpoint with an index between [0,getNumCheckpoints())
+        void loadCheckpoint_(uint64_t checkpoint_idx);
+
+        //! \brief Database instance
+        simdb::DatabaseManager *const db_mgr_;
+
+        //! \brief ArchData's from the roots given to our ctor
+        const std::vector<ArchData*> adatas_;
+
+        //! \brief Hold onto one checkpoint window for performance
+        std::unique_ptr<ChkptWindow> cached_window_;
+
+        //! \brief Hold onto a linear history of arch IDs and their ticks
+        std::vector<ArchIdsForTick> tick_runs_;
+
+        //! \brief Max linear index into the unique (arch ID, tick) pair
+        //! \note This is the total number of committed checkpoints
+        uint64_t num_checkpoints_ = 0;
+    };
+
+    /*!
+     * \class DatabaseCheckpointReplayer
+     * \brief To assist with post-simulation replay, this class implements
+     * a step() method
+     */
+    class DatabaseCheckpointReplayer
+    {
+    public:
+        //! \brief Construct with one root from which we'll find the ArchData's
+        DatabaseCheckpointReplayer(simdb::DatabaseManager* db_mgr, TreeNode* root) :
+            DatabaseCheckpointReplayer(db_mgr, std::vector<TreeNode*>({root}))
+        {}
+
+        //! \brief Construct with multiple roots from which we'll find the ArchData's
+        DatabaseCheckpointReplayer(simdb::DatabaseManager* db_mgr, const std::vector<TreeNode*>& roots) :
+            chkpt_loader_(db_mgr, roots)
+        {}
+
+        //! \brief Get the total number of committed checkpoints
+        uint64_t getNumCheckpoints() const { return chkpt_loader_.getNumCheckpoints(); }
+
+        //! \brief Go to the next checkpoint and fill in the ArchData's from the roots
+        //! \return Returns true if successful, false if we have no more checkpoints
+        bool step();
+
+    private:
+        //! \brief Checkpoint loader to do the heavy lifting
+        DatabaseCheckpointLoader chkpt_loader_;
+
+        //! \brief Linear index of the next checkpoint to load
+        uint64_t next_checkpoint_idx_ = 0;
+    };
 
     /*!
      * \brief Get the total number of checkpoints sent to the database thus far.
@@ -174,15 +292,18 @@ public:
         arch_id_t end_arch_id = UINT64_MAX;
         tick_t start_tick = UINT64_MAX;
         tick_t end_tick = UINT64_MAX;
-        size_t num_chkpts = 0;
         std::vector<char> chkpt_bytes;
     };
 
 private:
     FastCheckpointer checkpointer_;
     simdb::DatabaseManager* db_mgr_ = nullptr;
-    simdb::ConcurrentQueue<ChkptWindow>* pipeline_head_ = nullptr;
+    simdb::ConcurrentQueue<ChkptWindow>* chkpt_window_head_ = nullptr;
+    simdb::ConcurrentQueue<ArchIdsForTick>* tick_runs_head_ = nullptr;
+    std::queue<ArchIdsForTick> tick_runs_;
+    uint64_t committed_arch_id_ = 0;
     std::unique_ptr<simdb::pipeline::Flusher> pipeline_flusher_;
+    std::vector<chkpt_id_t> committed_checkpoints_;
 };
 
 } // namespace sparta::serialization::checkpoint
--- sparta/test/FastCheckpoint/CherryPickFastCheckpoint/CherryPickFastCheckpoint_test.cpp
+++ sparta/test/FastCheckpoint/CherryPickFastCheckpoint/CherryPickFastCheckpoint_test.cpp
@@ -18,6 +18,8 @@ using sparta::memory::MemoryObject;
 using sparta::memory::BlockingMemoryObjectIFNode;
 using sparta::serialization::checkpoint::CherryPickFastCheckpointer;
 using chkpt_id_t = typename CherryPickFastCheckpointer::chkpt_id_t;
+using ChkptWindow = CherryPickFastCheckpointer::ChkptWindow;
+using checkpoint_type = typename CherryPickFastCheckpointer::checkpoint_type;
 
 static const uint16_t HINT_NONE=0;
 
@@ -48,6 +50,27 @@ public:
     {}
 };
 
+//! Globals to be shared across test functions
+static const std::string db_file = "test.db";
+static std::unordered_map<chkpt_id_t, std::pair<uint32_t, uint32_t>> checkpoint_reg_values;
+static std::vector<std::pair<uint32_t, uint32_t>> committed_checkpoint_reg_values;
+
+void commitCurrentBranch(CherryPickFastCheckpointer& dbcp, std::vector<chkpt_id_t>& committed_checkpoints)
+{
+    dbcp.commitCurrentBranch(false, &committed_checkpoints);
+    for (auto chkpt_id : committed_checkpoints) {
+        committed_checkpoint_reg_values.push_back(checkpoint_reg_values[chkpt_id]);
+    }
+}
+
+void commitCurrentBranchWithNewHead(CherryPickFastCheckpointer& dbcp, std::vector<chkpt_id_t>& committed_checkpoints)
+{
+    dbcp.commitCurrentBranch(true, &committed_checkpoints);
+    for (auto chkpt_id : committed_checkpoints) {
+        committed_checkpoint_reg_values.push_back(checkpoint_reg_values[chkpt_id]);
+    }
+}
+
 void RunCheckpointerTest()
 {
     sparta::Scheduler sched;
@@ -76,7 +99,7 @@ void RunCheckpointerTest()
 
     simdb::AppManagers app_mgrs;
     app_mgrs.registerApp<CherryPickFastCheckpointer>();
-    auto& app_mgr = app_mgrs.createAppManager("test.db", true /*new file*/);
+    auto& app_mgr = app_mgrs.createAppManager(db_file, true /*new file*/);
 
     // Setup...
     // Apps must be enabled prior to parameterizing their custom factories
@@ -101,18 +124,23 @@ void RunCheckpointerTest()
         // Writing to the registers before checkpointing is not really necessary,
         // since we are not validating the values after loadCheckpoint(). That
         // functionality is already covered by the FastCheckpointer test.
-        uint32_t r1_val = rand();
-        uint32_t r2_val = rand();
+        uint32_t r1_val = rand() % 100;
+        uint32_t r2_val = rand() % 100;
 
         r1->write<uint32_t>(r1_val);
         r2->write<uint32_t>(r2_val);
 
         auto chkpt_id = fcp.createCheckpoint(force_snapshot);
+        checkpoint_reg_values[chkpt_id] = std::make_pair(r1_val, r2_val);
         return chkpt_id;
     };
 
+    // Data structure to keek track of the committed checkpoint IDs so we
+    // can validate the register values in the next test.
+    std::vector<chkpt_id_t> committed_checkpoints;
+
     // Make sure calling commitCurrentBranch() does nothing when we have no checkpoints.
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 0);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 0);
     EXPECT_EQUAL(fcp.getNumSnapshots(), 0);
@@ -120,7 +148,7 @@ void RunCheckpointerTest()
 
     // Calling commitCurrentBranch(true) to force a new head checkpoint should also
     // do nothing when we have no checkpoints at all.
-    dbcp.commitCurrentBranch(true);
+    commitCurrentBranchWithNewHead(dbcp, committed_checkpoints);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 0);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 0);
     EXPECT_EQUAL(fcp.getNumSnapshots(), 0);
@@ -146,7 +174,7 @@ void RunCheckpointerTest()
     //
     // Current chain before calling this method:
     //   S1->D1->D2->D3
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
 
     // Since the previous commit was a no-op, we should not have anything
     // in the database yet. The chain is still S1->D1->D2->D3
@@ -175,7 +203,7 @@ void RunCheckpointerTest()
     // disk without forcing a new head checkpoint should still be a
     // no-op. Current chain before/after this no-op is thus:
     //   S1->D1->D2->D3->D4->D5
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 0);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 6);
     EXPECT_EQUAL(fcp.getNumSnapshots(), 1);
@@ -187,7 +215,7 @@ void RunCheckpointerTest()
     // Everything checkpointed thus far should be in the database,
     // and the FastCheckpointer should only have the new S2 snapshot
     // that we just forced.
-    dbcp.commitCurrentBranch(true);
+    commitCurrentBranchWithNewHead(dbcp, committed_checkpoints);
     auto S2 = fcp.getHeadID();
     EXPECT_SNAPSHOT(S2);
 
@@ -245,7 +273,7 @@ void RunCheckpointerTest()
     // If we serialize now without forcing a new head checkpoint, we should only
     // be able to write to disk S2 through D10, and S3 through D13 should remain
     // in the fast checkpointer with S3 as the new head checkpoint.
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 12); // S1 through D10 (added S2 through D10)
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 4);   // S3 through D13 (must retain S3, did not force)
     EXPECT_EQUAL(fcp.getNumSnapshots(), 1);     // S3 only
@@ -316,7 +344,7 @@ void RunCheckpointerTest()
     // The only thing remaining in the fast checkpointer
     // is S4 (current), while the database got six new
     // checkpoints S3->D11->D12->D13->D14->D15
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 18); // S1-S3, D1-D15
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 1);   // S4 only
     EXPECT_EQUAL(fcp.getNumSnapshots(), 1);     // S4 snapshot
@@ -330,7 +358,7 @@ void RunCheckpointerTest()
     // Try to serialize to disk without forcing a new head checkpoint,
     // which is a no-op here since the fast checkpointer is only holding
     // onto S4 (the only snapshot - it can't get rid of it).
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 18);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 1);
     EXPECT_EQUAL(fcp.getHeadID(), S4);
@@ -359,7 +387,7 @@ void RunCheckpointerTest()
     // Commit the current branch without forcing a new head checkpoint.
     // S5 should then be the new head and all other 6 checkpoints should
     // be flushed to disk.
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 24);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 1);
     EXPECT_EQUAL(fcp.getHeadID(), S6);
@@ -384,7 +412,7 @@ void RunCheckpointerTest()
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 7);
 
     // Commit the current branch and force a new head snapshot S8
-    dbcp.commitCurrentBranch(true);
+    commitCurrentBranchWithNewHead(dbcp, committed_checkpoints);
     auto S8 = fcp.getHeadID();
     EXPECT_SNAPSHOT(S8);
 
@@ -417,7 +445,7 @@ void RunCheckpointerTest()
     // Commit the current branch without forcing a new head checkpoint.
     // The remaining branch in the fast checkpointer should be:
     //   S8 -> D31->D32->D33->D34
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 5);
     EXPECT_EQUAL(fcp.getHeadID(), S8);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 31); // Did not get any new chkpts
@@ -425,7 +453,7 @@ void RunCheckpointerTest()
     EXPECT_EQUAL(fcp.findCheckpoint(D34)->getRestoreChain().size(), 5);
 
     // Commit the current branch and force a new head checkpoint S10
-    dbcp.commitCurrentBranch(true);
+    commitCurrentBranchWithNewHead(dbcp, committed_checkpoints);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 1);
 
     auto S10 = fcp.getHeadID();
@@ -464,7 +492,7 @@ void RunCheckpointerTest()
     // We should be left with:
     //   S11 -> D37       (fast checkpointer)
     //   S10 -> D35->D36  (added to DB)
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 2);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 39); // Got 3 new chkpts
     EXPECT_EQUAL(fcp.getCheckpointChain(D37).size(), 2);
@@ -501,7 +529,7 @@ void RunCheckpointerTest()
     // We should be left with:
     //   S13 -> D46                      (fast checkpointer)
     //   S11 -> D37->D43 -> S12 -> D44   (added to DB)
-    dbcp.commitCurrentBranch();
+    commitCurrentBranch(dbcp, committed_checkpoints);
     EXPECT_EQUAL(fcp.getNumCheckpoints(), 2);
     EXPECT_EQUAL(fcp.getHeadID(), S13);
     EXPECT_EQUAL(dbcp.getNumCheckpoints(), 44); // Got 5 new chkpts
@@ -514,9 +542,55 @@ void RunCheckpointerTest()
     clocks.enterTeardown();
 }
 
+void RunPostSimCheckpointerTest()
+{
+    // Build the same simulator as the first test
+    sparta::Scheduler sched;
+    RootTreeNode clocks("clocks");
+    sparta::Clock clk(&clocks, "clock", &sched);
+
+    // Create a tree with some register sets and memory
+    RootTreeNode root;
+
+    DummyDevice dummy(&root);
+    std::unique_ptr<RegisterSet> rset(RegisterSet::create(&dummy, reg_defs));
+
+    DummyDevice dummy2(&dummy);
+    std::unique_ptr<RegisterSet> rset2(RegisterSet::create(&dummy2, reg_defs));
+
+    root.enterConfiguring();
+    root.enterFinalized();
+    sched.finalize();
+
+    auto r1 = rset->getRegister("reg2");
+    auto r2 = rset2->getRegister("reg2");
+    sparta_assert(r1 != r2);
+
+    // Attach to the existing database file
+    simdb::DatabaseManager db_mgr(db_file, false /*not a new file*/);
+
+    // Verify that checkpoints can be reloaded. Use the DatabaseCheckpointReplayer
+    // since that implicitly covers the DatabaseCheckpointLoader.
+    CherryPickFastCheckpointer::DatabaseCheckpointReplayer replayer(&db_mgr, &root);
+    EXPECT_EQUAL(replayer.getNumCheckpoints(), committed_checkpoint_reg_values.size());
+
+    size_t count = 0;
+    for (const auto& [expected_r1_val, expected_r2_val] : committed_checkpoint_reg_values) {
+        EXPECT_TRUE(replayer.step());
+        //EXPECT_EQUAL(r1->read<uint32_t>(), expected_r1_val);
+        //EXPECT_EQUAL(r2->read<uint32_t>(), expected_r2_val);
+        if (r1->read<uint32_t>() != expected_r1_val || r2->read<uint32_t>() != expected_r2_val) {
+            std::cout << "Failed at count " << count << std::endl;
+        }
+        ++count;
+    }
+    EXPECT_FALSE(replayer.step()); // No more checkpoints to replay
+}
+
 int main()
 {
     RunCheckpointerTest();
+    RunPostSimCheckpointerTest();
 
     REPORT_ERROR;
     return ERROR_CODE;
